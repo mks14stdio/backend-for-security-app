12010144fd3cf816e62763f1204c2f7335b25ec2
Denis Sova <dsova@sfedu.ru>
2025-11-23 18:03:24 +0300

    Refactor dependecy

--- backend/src/api/dependecy.py
+++ backend/src/api/dependecy.py
@@ -1,47 +1,49 @@
-from typing import Annotated
-
-import jwt
+from typing import Annotated, Any
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-
-from src.repository.user_repository import UserRepository
-from src.scheme.user import UserRead
-from src.service.auth_service import AuthService
-from src.service.user_service import UserService
-
 from sqlalchemy.ext.asyncio import AsyncSession
-from src.database.db import create_session_db
 
-from ..security import decode_token
+from src.database.db import create_session_db
+from src.repository.user_repository import UserRepository
+from src.service.user_service import UserService
+from src.service.auth_service import AuthService
+from src.scheme.user import UserRead, UserRole
+
 
 SessionDep = Annotated[AsyncSession, Depends(create_session_db)]
 
+
+
 def get_user_service(session: SessionDep) -> UserService:
-    return UserService(UserRepository(session))
+    repo = UserRepository(session=session)
+    return UserService(repository=repo, session=session)
 
 UserServiceDep = Annotated[UserService, Depends(get_user_service)]
 
+
+
 def get_auth_service(session: SessionDep) -> AuthService:
-    return AuthService(UserRepository(session))
+    repo = UserRepository(session=session)
+    return AuthService(repo)
 
 AuthServiceDep = Annotated[AuthService, Depends(get_auth_service)]
+
+
 
 security = HTTPBearer()
 SecurityDep = Annotated[HTTPAuthorizationCredentials, Depends(security)]
 
-def get_user(token: SecurityDep, auth_service: AuthServiceDep):
+
+def get_user(token: SecurityDep, auth_service: AuthServiceDep) -> dict[str, Any]:
     return auth_service.get_currect_user(token.credentials)
 
-GetUserDep = Annotated[UserRead, Depends(get_user)]
+GetUserDep = Annotated[dict[str, Any], Depends(get_user)]
 
-from src.scheme.user import UserRole
 
 def require_role(roles: list[UserRole]):
-    def role_checker(user: GetUserDep):
-        if user.role not in roles:
-            raise HTTPException(
-                status_code=403,
-                detail="Недостаточно прав"
-            )
+    def checker(user: GetUserDep):
+        if user["role"] not in roles:
+            raise HTTPException(403, "Недостаточно прав")
         return user
-    return Depends(role_checker)
+
+    return Depends(checker)
