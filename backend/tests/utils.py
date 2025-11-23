2ba433b41751e447522b6a4bd6e033071dd19818
Denis Sova <dsova@sfedu.ru>
2025-11-23 18:24:30 +0300

    Remove uitls

--- backend/tests/utils.py
+++
@@ -1,9 +0,0 @@
-from httpx import AsyncClient
-from tests.conftest import client
-from src.scheme.user import UserRegister
-
-async def create_user(*, name: str, password: str, email: str) -> UserRegister:
-    return UserRegister(
-        full_name=name, password=password, email=email
-    )
-
