a7691ccdf5ef1ca4ebdf2e9a3036d378c422a477
Denis Sova <dsova@sfedu.ru>
2025-12-03 08:46:19 +0300

    Make repository flush, add delete, find all and update find one

--- backend/src/repository/article_repository.py
+++ backend/src/repository/article_repository.py
@@ -1,9 +1,12 @@
 
 import select
-from tkinter import ARC
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import insert, update, select
+from typing import List
 
+from httpx import delete
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import insert, update, select, delete
+
+from src.models import article
 from src.models.article import Article
 
 class ArticleRepository:
@@ -14,10 +17,10 @@ class ArticleRepository:
     async def add_one(self, data: dict) -> Article:
         stmt = insert(Article).values(**data).returning(Article)
         result = await self.session.execute(stmt)
-        await self.session.commit()
+        await self.session.flush()
 
         return result.scalar_one()
-    
+
     async def update_one(self, id: int, data: dict) -> Article:
         stmt = (
             update(Article)
@@ -27,11 +30,19 @@ class ArticleRepository:
         )
         result = await self.session.execute(stmt)
         
-        await self.session.commit()
-
+        await self.session.flush()
         return result.scalar_one()
     
-    async def find_one(self, id: int) -> Article | None:
-        stmt = select(Article).where(Article.id == id)
+    async def delete_article(self, id: int) -> None:
+        stmt = delete(Article).where(Article.id == id)
+        await self.session.execute(stmt)
+        await self.session.flush()
+
+    async def find_all(self, limit: int = 10, offset: int = 0) -> List[Article]:
+        stmt = select(Article).limit(limit).offset(offset)
         result = await self.session.execute(stmt)
-        return result.scalar_one_or_none()
+        return [i for i in result.scalars().all()]
+    
+
+    async def find_one(self, id: int) -> Article | None:
+        return await self.session.get(Article, id)
