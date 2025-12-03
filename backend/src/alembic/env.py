7ede7040b7f9015491e77b11d22d49e1bcfa4f81
Denis Sova <dsova@sfedu.ru>
2025-12-03 08:48:03 +0300

    Update: Alembic stuff

--- backend/src/alembic/env.py
+++ backend/src/alembic/env.py
@@ -23,9 +23,12 @@ config.set_main_option("sqlalchemy.url", settings.DATABASE_URL + "?async_fallbac
 
 from src.scheme.user import UserRole #noqa
 from src.models.users import User #noqa
+from src.models.module import Module, ModuleItem #noqa
 from src.models.article import Article #noqa
 from src.models.question import Question #noqa
 from src.models.test import Test #noqa
+
+from src.models.token import RefreshToken, TestSessionToken #noqa
 
 from src.database.db import Base
 
