4da30e6bf19b908ec70889783a4d519987b4bd45
Denis Sova <denissova6@gmail.com>
2026-01-29 13:57:03 +0300

    Refactor: remove test end-points

--- backend/src/api/router.py
+++ backend/src/api/router.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter
 
-from .routers import auth
-from .routers import user
-from .routers import article # type: ignore
-from .routers import test
-from .routers import module
+from .routers import (
+    article,  # type: ignore
+    auth,
+    module,
+    user,
+)
 
 main_router = APIRouter()
 main_router.include_router(auth.router)
 main_router.include_router(user.router)
 main_router.include_router(article.router)
-main_router.include_router(test.router)
+
 main_router.include_router(module.router)
