e17695048247d89782b56c5e9a379a3d6d1b5716
Denis Sova <denissova6@gmail.com>
2026-01-29 13:56:27 +0300

    Refactor: remove test session models

--- backend/src/models/token.py
+++ backend/src/models/token.py
@@ -1,21 +1,9 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
 from src.database.db import Base
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 
 class RefreshToken(Base):
     __tablename__ = "refresh_tokens"
 
     token: Mapped[str] = mapped_column(primary_key=True)
-
-class TestSessionToken(Base):
-    __tablename__ = "test_session_tokens"
-
-    token: Mapped[str] =  mapped_column(primary_key=True)
-
-class TestSessionItem(Base):
-    __tablename__ = "test_session_items"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-
-    test_session_token: Mapped[str] = mapped_column(ForeignKey("test_session_tokens.token", ondelete="CASCADE"))
-    question_id: Mapped[int] = mapped_column(ForeignKey("questions.id", ondelete="SET NULL"))
