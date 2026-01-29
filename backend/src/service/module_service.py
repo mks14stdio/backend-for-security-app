2d12f21345362484a1b4a37828be0f902b0eebff
Denis Sova <denissova6@gmail.com>
2026-01-29 14:02:21 +0300

    Little refactor

--- backend/src/service/module_service.py
+++ backend/src/service/module_service.py
@@ -1,21 +1,21 @@
-
-from ast import Not
-from curses.ascii import HT
-from unittest.mock import Base
 from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.models.module import ModuleItem
 from src.repository.module_item_repository import ModuleItemRepository
-from src.models import Module
 from src.repository.module_repository import ModuleRepository
 from src.scheme.module import (
-    ModuleBase, ModuleCreate, ModuleItemBase, ModuleRead, ModuleUpdate,
-    ModuleItemCreate, ModuleItemRead, ModuleItemUpdate)
+    ModuleBase,
+    ModuleCreate,
+    ModuleItemBase,
+    ModuleItemCreate,
+    ModuleItemRead,
+    ModuleItemUpdate,
+    ModuleRead,
+    ModuleUpdate,
+)
 
 
 class ModuleService:
-
     def __init__(self, session: AsyncSession) -> None:
         self.repository = ModuleRepository(session=session)
         self.item_repository = ModuleItemRepository(session=session)
@@ -23,9 +23,19 @@ class ModuleService:
 
     async def add_module(self, data: ModuleCreate):
         try:
-            module = await self.repository.add_one(ModuleBase(title=data.title).model_dump())
+            module = await self.repository.add_one(
+                ModuleBase(title=data.title).model_dump()
+            )
 
-            item_base = [ModuleItemBase(title=n.title, article_id=n.article_id, order_index=i, module_id=module.id).model_dump() for i, n in enumerate(data.items)]
+            item_base = [
+                ModuleItemBase(
+                    title=n.title,
+                    article_id=n.article_id,
+                    order_index=i,
+                    module_id=module.id,
+                ).model_dump()
+                for i, n in enumerate(data.items)
+            ]
             await self.item_repository.add_all(item_base)
 
             await self.session.commit()
@@ -35,7 +45,7 @@ class ModuleService:
             await self.session.rollback()
             print(e)
             raise HTTPException(400, "Не удалось создать модуль")
-        
+
     async def get_one(self, id: int) -> ModuleRead:
         try:
             result = await self.repository.get_one(id)
@@ -45,7 +55,9 @@ class ModuleService:
 
     async def update_module_item(self, data: ModuleItemUpdate, item_id: int):
         try:
-            module_item = await self.item_repository.update_one(item_id, data.model_dump())
+            module_item = await self.item_repository.update_one(
+                item_id, data.model_dump()
+            )
             await self.session.commit()
             return ModuleItemRead.model_validate(module_item)
         except Exception as e:
@@ -56,18 +68,20 @@ class ModuleService:
         try:
             delete_module_item = await self.item_repository.get_by_id(item_id)
 
-            if (delete_module_item.module_id != id):
+            if delete_module_item.module_id != id:
                 raise HTTPException(400, "Нельзя удалить элемент из другого модуля")
 
             order_index = delete_module_item.order_index
-            
+
             await self.item_repository.delete(item_id)
-            
+
             module_items = await self.item_repository.get_all_by_module_id(id)
 
             item_to_update = filter(lambda x: x.order_index > order_index, module_items)
             for item in item_to_update:
-                await self.item_repository.update_one(item.id, {"order_index": item.order_index - 1})
+                await self.item_repository.update_one(
+                    item.id, {"order_index": item.order_index - 1}
+                )
 
             await self.session.commit()
             return {"message": "Item module deleted successfully"}
@@ -79,15 +93,20 @@ class ModuleService:
     async def add_module_item(self, data: ModuleItemCreate, module_id: int):
         try:
             len_module = len(await self.item_repository.get_all_by_module_id(module_id))
-            module_item = await self.item_repository.add_one(ModuleItemBase(module_id=module_id, title=data.title, 
-                                                                            article_id=data.article_id, 
-                                                                            order_index=len_module).model_dump())
+            module_item = await self.item_repository.add_one(
+                ModuleItemBase(
+                    module_id=module_id,
+                    title=data.title,
+                    article_id=data.article_id,
+                    order_index=len_module,
+                ).model_dump()
+            )
             await self.session.commit()
             return ModuleItemRead.model_validate(module_item)
         except Exception as e:
             await self.session.rollback()
             raise HTTPException(400, "Не удалось добавить элемент в модуль")
-        
+
     async def update_module(self, data: ModuleUpdate, module_id: int):
         try:
             module = await self.repository.update_module(data.model_dump(), module_id)
