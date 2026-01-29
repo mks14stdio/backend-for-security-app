4323a803b82cdd36f783f903a37d8b46f4494b53
Denis Sova <denissova6@gmail.com>
2026-01-29 13:57:46 +0300

    Refactor: remove frontend

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-// Импортируем Tailwind CSS
-import './style.css'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
