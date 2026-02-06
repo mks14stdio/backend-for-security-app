fdc5d5c7f6b24543f5443701c9454c4e0d5ede56
Denis Sova <denissova6@gmail.com>
2026-02-06 13:33:00 +0300

    Update __init__.py

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -1,19 +1,15 @@
-from src.models.module import Module, ModuleItem
+from src.models.achievement import Achievement, AchievementUser
 from src.models.article import Article
-from src.models.question import Question, QuestionAnswer
-from src.models.test import Test
-from src.models.token import RefreshToken, TestSessionToken, TestSessionItem
+from src.models.module import Module, ModuleItem
+from src.models.token import RefreshToken
 from src.models.users import User
 
 __all__ = [
-    'Module',
-    'ModuleItem',
-    'Article',
-    'Question',
-    'QuestionAnswer',
-    'Test',
-    'RefreshToken',
-    'TestSessionToken',
-    'TestSessionItem',
-    'User'
+    "Module",
+    "ModuleItem",
+    "Article",
+    "RefreshToken",
+    "User",
+    "Achievement",
+    "AchievementUser",
 ]
