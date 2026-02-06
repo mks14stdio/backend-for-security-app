052d37d0e876f4ac20579bb6907ebfeac8bb8806
Denis Sova <denissova6@gmail.com>
2026-02-06 13:29:12 +0300

    Delete startup.sh

--- backend/scripts/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -d \
-      --name swag_db \
-      -e POSTGRES_USER=swag \
-      -e POSTGRES_PASSWORD=qwe123 \
-      -e POSTGRES_DB=db \
-      -p 5432:5432 \
-      postgres
