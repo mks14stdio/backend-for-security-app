85ec8b3639432b59d85e4cded80554d8289ffc4a
Denis Sova <denissova6@gmail.com>
2026-02-06 13:31:26 +0300

    Update user_service.py

--- backend/src/service/user_service.py
+++ backend/src/service/user_service.py
@@ -1,57 +1,50 @@
-from typing import List
-
 from fastapi import HTTPException
 from pydantic import EmailStr
 from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.scheme.auth import RegisterSchema
 from src.repository.user_repository import UserRepository
+from src.scheme.auth import RegisterSchema
 
-from ..models.users import User
-from ..scheme.user import UserCreate, UserRead, UserRole
-
-
+from ..models.users import User, UserProfile
+from ..scheme.user import UserRead, UserRole
 from ..security import get_password_hash
 
-class UserService:
 
+class UserService:
     def __init__(self, session: AsyncSession):
-        self.repository: UserRepository = UserRepository(session) 
+        self.repository: UserRepository = UserRepository(session)
         self.session: AsyncSession = session
 
+    async def create_user(
+        self, new_user: RegisterSchema, role: UserRole = UserRole.USER
+    ) -> UserRead:
+        if await self.repository.find_by_email(new_user.email):
+            raise HTTPException(status_code=400, detail="Email already registered")
 
-    async def create_user(self, new_user: RegisterSchema, role: UserRole = UserRole.USER) -> UserRead:
-        
-        if await self.repository.find_one_by_email(new_user.email):
-           raise HTTPException(status_code=400, detail="Email already registered")
-        
         try:
-            user_payload = {
-                "email": new_user.email,
-                "hashed_password": get_password_hash(new_user.password),
-                "role": role
-            }
-            
-            user = await self.repository.add_one(user_payload)
+            hashed_password = get_password_hash(new_user.password)
+            user = User(
+                hashed_password=hashed_password, email=new_user.email, role=role
+            )
 
-            profile = {
-                "first_name": new_user.first_name,
-                "last_name": new_user.last_name,
-                "age": new_user.age,
-                "gender": new_user.gender,
-                "user_id": user.id
-            }
-            await self.repository.add_profile_user(profile)
+            user.profile = UserProfile(
+                first_name=new_user.first_name,
+                last_name=new_user.last_name,
+                age=new_user.age,
+                gender=new_user.gender,
+            )
+
+            await self.repository.add(user)
+
             await self.session.commit()
+            await self.session.refresh(user)
 
-            return await self.get_user_by_email(new_user.email)
+            return UserRead.model_validate(user)
         except Exception as e:
             await self.session.rollback()
             raise e
-            
-
 
     async def get_user_by_email(self, email: EmailStr) -> UserRead:
-        user = await self.repository.find_one_by_email(email)
+        user = await self.repository.find_by_email(email)
         return UserRead.model_validate(user)
