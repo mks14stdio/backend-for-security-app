b818dc3ca5493050da9452ef819c0f71f333cdbf
Denis Sova <denissova6@gmail.com>
2026-02-06 13:30:56 +0300

    Add default for db parametrs (for dev only)

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -4,26 +4,29 @@ from pydantic_settings import BaseSettings
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 class Settings(BaseSettings):
-    DB_USER: str
-    DB_PASSWORD: str
-    DB_HOST: str
-    DB_PORT: int
-    DB_NAME: str
+    DB_USER: str = "user_dev"
+    DB_PASSWORD: str = "qweasdzxc123"
+    DB_HOST: str = "db"
+    DB_PORT: int = 5432
+    DB_NAME: str = "test_dev"
+
+    ENV_TYPE: str
 
     @property
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-
-    PRIVATE_KEY_PATH: Path = BASE_DIR / 'certs' / 'private-key.pem'
-    PUBLIC_KEY_PATH: Path = BASE_DIR / 'certs' / 'public-key.pem'
-    JWT_ALGORITHM: str = 'PS256'
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30 #TODO: MAKE IT 5 minute
+    PRIVATE_KEY_PATH: Path = BASE_DIR / "certs" / "private-key.pem"
+    PUBLIC_KEY_PATH: Path = BASE_DIR / "certs" / "public-key.pem"
+    JWT_ALGORITHM: str = "PS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30  # TODO: MAKE IT 5 minute
     REFRESH_TOKEN_EXPIRE_DAYS: int = 30
 
     TEST_TOKEN_EXPIRE_MUNUTES: int = 15
 
     API_URL: str = "/v1"
 
-settings = Settings() # type: ignore
+
+settings = Settings()  # pyright: ignore[reportCallIssue]
