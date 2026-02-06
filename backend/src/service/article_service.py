b0bf8efb778a2b450fe19a5ce9a5baece47a16a0
Denis Sova <denissova6@gmail.com>
2026-02-06 14:43:57 +0300

    Update Article return

--- backend/src/service/article_service.py
+++ backend/src/service/article_service.py
@@ -14,24 +14,24 @@ class ArticleService:
         self.repository: ArticleRepository = ArticleRepository(session)
         self.session: AsyncSession = session
 
-    async def add(self, article: ArticleCreate) -> ArticleRead:
+    async def add(self, article: ArticleCreate) -> Article:
         try:
             new_article: Article = Article(title=article.title, content=article.content)
             await self.repository.add(new_article)
             await self.session.commit()
-            return ArticleRead.model_validate(new_article, from_attributes=True)
+            return new_article
         except Exception as e:
             await self.session.rollback()
             raise HTTPException(status_code=400, detail=str(e))
 
-    async def get_all(self, limit: int = 10, offset: int = 0) -> List[ArticleRead]:
+    async def get_all(self, limit: int = 10, offset: int = 0) -> List[Article]:
         items = await self.repository.find_all(limit, offset)
-        return [ArticleRead.model_validate(i, from_attributes=True) for i in items]
+        return items
 
-    async def get_one_by_id(self, id: int) -> ArticleRead:
-        find_article = await self.repository.find(id)
-        if find_article:
-            return ArticleRead.model_validate(find_article, from_attributes=True)
+    async def get_one_by_id(self, id: int) -> Article:
+        founded_article = await self.repository.find(id)
+        if founded_article:
+            return founded_article
         raise HTTPException(status_code=404, detail="Статья не найдена")
 
     async def delete(self, id: int):
@@ -47,7 +47,7 @@ class ArticleService:
             await self.session.rollback()
             raise HTTPException(status_code=404, detail=str(e))
 
-    async def update(self, id: int, article: ArticleUpdate) -> ArticleRead:
+    async def update(self, id: int, article: ArticleUpdate) -> Article:
         try:
             article_to_update: Article | None = await self.repository.find(id)
 
@@ -61,7 +61,7 @@ class ArticleService:
 
             await self.session.commit()
             await self.session.refresh(article_to_update)
-            return ArticleRead.model_validate(article_to_update, from_attributes=True)
+            return article_to_update
         except NoResultFound:
             await self.session.rollback()
             raise HTTPException(status_code=404, detail=f"Статья с id={id} не найдена")
