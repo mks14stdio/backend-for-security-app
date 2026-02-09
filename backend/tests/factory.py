c0ca8ad2a857be7608f23200bbd659653c72d1e9
Denis Sova <denissova6@gmail.com>
2026-02-09 14:11:55 +0300

    Add create_module utils

--- backend/tests/factory.py
+++ backend/tests/factory.py
@@ -3,7 +3,9 @@ from sqlalchemy.ext.asyncio.session import AsyncSession
 from src.database.db import Base
 from src.models.achievement import Achievement
 from src.models.article import Article
-from src.models.module import Module
+from src.models.module import Module, ModuleItem
+from src.models.quiz import Answer, Question, QuizArticle
+from src.scheme.quiz import AnswerType
 
 
 async def push_to_database(db: AsyncSession, model: Base):
@@ -22,10 +24,25 @@ async def create_article(db: AsyncSession, **kwargs) -> Article:
     return await push_to_database(db, article)  # pyright: ignore[reportReturnType]
 
 
-async def create_module(db: AsyncSession, **kwargs) -> Module:
+async def create_module(
+    db: AsyncSession,
+    *,
+    description: str | None = None,
+    items: list[Article] | None = None,
+) -> Module:
+
+    if not items:
+        items = [await create_article(db) for i in range(5)]
+
+    real_items = [
+        ModuleItem(article=article, article_id=article.id, order_index=index)
+        for index, article in enumerate(items)
+    ]
+
     module = Module(
-        need_to_unlook=kwargs.get("xp", 0),  # FREE
-        title=kwargs.get("title", "Module Title"),
+        title="Module Title",
+        description=description,
+        items=real_items,
     )
 
     return await push_to_database(db, module)  # pyright: ignore[reportReturnType]
@@ -42,4 +59,18 @@ async def create_achievement(db: AsyncSession, **kwargs) -> Achievement:
     return await push_to_database(db, achievement)  # pyright: ignore[reportReturnType]
 
 
-async def create_quiz(db: AsyncSession, article: Article, **kwargs): ...
+async def create_quiz(db: AsyncSession, article: Article, **kwargs):
+    quiz = QuizArticle(article=article, question_count=10)
+
+    for i in range(10):
+        question = Question(
+            text="Is True?",
+            type=AnswerType.SINGLE,
+            answers=[
+                Answer(text="True", is_correct=True),
+                Answer(text="False", is_correct=False),
+            ],
+        )
+        quiz.questions.append(question)
+
+    return await push_to_database(db, quiz)  # pyright: ignore[reportReturnType]
