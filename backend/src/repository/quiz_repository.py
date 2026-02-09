bf32caf8aff1776c1062983994641f5ce15f2602
Denis Sova <denissova6@gmail.com>
2026-02-09 14:09:41 +0300

    Change a my big mistake

--- backend/src/repository/quiz_repository.py
+++ backend/src/repository/quiz_repository.py
@@ -1,20 +1,20 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.models.module import Module
+from src.models.quiz import QuizArticle
 
 
 class QuizRepository:
     def __init__(self, session: AsyncSession) -> None:
         self.session = session
 
-    async def add(self, module: Module) -> Module:
-        self.session.add(module)
+    async def add(self, quiz: QuizArticle) -> QuizArticle:
+        self.session.add(quiz)
         await self.session.flush()
-        return module
+        return quiz
 
-    async def get(self, id: int) -> Module | None:
-        return await self.session.get(Module, id)
+    async def get(self, id: int) -> QuizArticle | None:
+        return await self.session.get(QuizArticle, id)
 
-    async def delete(self, module: Module) -> None:
+    async def delete(self, module: QuizArticle) -> None:
         await self.session.delete(module)
         await self.session.flush()
