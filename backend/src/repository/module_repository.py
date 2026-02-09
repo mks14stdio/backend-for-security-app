e7cb3963dfb7d7b733b8a3d30acb0e112bca5a46
Denis Sova <denissova6@gmail.com>
2026-02-09 14:09:19 +0300

    Add get all modules

--- backend/src/repository/module_repository.py
+++ backend/src/repository/module_repository.py
@@ -1,4 +1,6 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.types import ASGIApp
 
 from src.models.module import Module
 
@@ -14,6 +16,11 @@ class ModuleRepository:
 
     async def get(self, id: int) -> Module | None:
         return await self.session.get(Module, id)
+
+    async def get_all(self, limit: int = 10, offset: int = 0) -> list[Module]:
+        stmt = select(Module).limit(limit).offset(offset)
+        result = await self.session.scalars(stmt)
+        return list(result)
 
     async def delete(self, module: Module) -> None:
         await self.session.delete(module)
